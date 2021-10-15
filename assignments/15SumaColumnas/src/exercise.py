b910c0cca3b3adb30e3e3d51b07739b4b05cacc2
Diego0103 <A01254073@itesm.mx>
2021-10-15 19:01:46 +0000

    versionfinal

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,13 +1,24 @@
 def main():
-matriz = []
-listadesumas=[]
-r = int(input())
-c = int(input())
-for i in range(r):
-    lista = []
-    matriz.append(lista)
-    for j in range(c):
-        matriz[i].append(input())
+    matriz = []
+    listadesumas=[]
+    r = int(input())
+    c = int(input())
+    if c<=0:
+        print('Error')
+    else:
+        for i in range(r):
+            lista = []
+            matriz.append(lista)
+            for j in range(c):
+                valor = int(input())
+                matriz[i].append(valor)
+    
+        for i in range(c):
+            suma=0
+            for j in range(r):
+                suma=matriz[j][i]+suma
+            listadesumas.append(suma)
+        print(listadesumas)
 
 
 if __name__=='__main__':
